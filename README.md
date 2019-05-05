7823fcf141efdcf2f9b6aa8c1fe86ae67b95fbf9
Yuki Ito <acomagu@gmail.com>
2019-05-06 00:27:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GitHub Timeline Expander
+
+Expand initially.
+
+![Screenshot](screenshot.png)
+
+## Installation to your browser
+
+This is a UserScript.
+
+In Chrome, Just drag and drop the script to extension page.
+
+![Installation](installation.png)
+
+[Download Script](../../raw/master/gte.user.js)
---
+++ gte.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        GitHub Timeline Expander
+// @namespace   acomagu/github-timeline-expander
+// @includes    https://github.com
+// ==/UserScript==
+
+new MutationObserver((mutationList, observer) => {
+  mutationList
+    .filter(m => m.type == 'childList')
+    .flatMap(m => [...m.addedNodes])
+    .filter(e => e instanceof Element)
+    .flatMap(e => [...e.querySelectorAll('.Details')])
+    .forEach(e => e.classList.add('Details--on', 'open'));
+}).observe(document.querySelector('.news'), { childList: true, subtree: true });
